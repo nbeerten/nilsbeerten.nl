c80e2359408df8beec5c4d53298aea5f3d7ed6c8
nbeerten <59444993+nbeerten@users.noreply.github.com>
2023-05-19 22:47:03 +0200

    Build fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,9 +4,6 @@
 </script>
 
 <svelte:head>
-    <link rel="icon" href="/favicon.ico" sizes="any">
-    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
     <meta name="theme-color" content="#0c0a09" media="(prefers-color-scheme: dark)">
     <meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)">
 </svelte:head>
