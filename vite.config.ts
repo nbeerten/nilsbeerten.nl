4c4b549f8efc5f07bce3c15e70be6560a38f0070
nbeerten <59444993+nbeerten@users.noreply.github.com>
2023-07-01 13:23:45 +0200

    Top level await error fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
-    plugins: [sveltekit()],
+    plugins: [sveltekit(), topLevelAwait()],
     define: {
         "import.meta.env.VERCEL_ANALYTICS_ID": JSON.stringify(process.env.VERCEL_ANALYTICS_ID)
     }
