e21cb808817ea340c26603203d697b1dd5ff20c4
nbeerten <59444993+nbeerten@users.noreply.github.com>
2023-07-01 13:28:18 +0200

    Revert "Top level await error fix"
    This reverts commit 4c4b549f8efc5f07bce3c15e70be6560a38f0070.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
-import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
-    plugins: [sveltekit(), topLevelAwait()],
+    plugins: [sveltekit()],
     define: {
         "import.meta.env.VERCEL_ANALYTICS_ID": JSON.stringify(process.env.VERCEL_ANALYTICS_ID)
     }
