7f98cc1d0ae1ed6aaddf7268340cc2a7941ce369
nbeerten <59444993+nbeerten@users.noreply.github.com>
2023-11-28 17:42:08 +0100

    Vercel

--- astro.config.ts
+++ astro.config.ts
@@ -3,25 +3,19 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import cloudflare from "@astrojs/cloudflare";
 import sitemap from "@astrojs/sitemap";
-
 import robotsTxt from "astro-robots-txt";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        tailwind(),
-        icon({
-            include: {
-                "simple-icons": ["*"],
-                lucide: ["*"],
-            },
-        }),
-        sitemap(),
-        robotsTxt(),
-    ],
-    site: "https://nilsbeerten.nl",
-    output: "hybrid" /* This makes astro-icon work (with this switched to "server", worker.js in the outputDir is over 1 MB, too large for Cloudflare) */,
-    adapter: cloudflare({
-        imageService: "passthrough",
-    }),
+  integrations: [tailwind(), icon({
+    include: {
+      "simple-icons": ["*"],
+      lucide: ["*"]
+    }
+  }), sitemap(), robotsTxt()],
+  site: "https://nilsbeerten.nl",
+  output: "hybrid" /* This makes astro-icon work (with this switched to "server", worker.js in the outputDir is over 1 MB, too large for Cloudflare) */,
+  adapter: vercel()
 });
