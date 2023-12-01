dae9cbfb37ac9c3e46f88b4b38ebea1d97d7c984
nbeerten <59444993+nbeerten@users.noreply.github.com>
2023-12-01 17:43:44 +0100

    Cloudflare + font experiment

--- astro.config.ts
+++ astro.config.ts
@@ -3,43 +3,27 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
-
-import vercel from "@astrojs/vercel/serverless";
-
-const VERCEL_PREVIEW_SITE = (process.env.VERCEL_ENV !== "production" &&	process.env.VERCEL_URL) ? `https://${process.env.VERCEL_URL}` : false;
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        tailwind(),
-        icon({
-            include: {
-                "simple-icons": ["github", "x", "discord"],
-                lucide: ["mail", "arrow-left"],
-            },
-        }),
-        sitemap({
-            filter: (page) =>
-                new URL(page).toString() !== new URL("https://nilsbeerten.nl/brand/").toString(),
-        }),
-        robotsTxt({
-            policy: [
-                {
-                    userAgent: "*",
-                    allow: "/",
-                    disallow: "/brand",
-                },
-            ],
-        }),
-    ],
-    site: VERCEL_PREVIEW_SITE || "https://nilsbeerten.nl",
+    integrations: [tailwind(), icon({
+        include: {
+            "simple-icons": ["github", "x", "discord"],
+            lucide: ["mail", "arrow-left"]
+        }
+    }), sitemap({
+        filter: page => new URL(page).toString() !== new URL("https://nilsbeerten.nl/brand/").toString()
+    }), robotsTxt({
+        policy: [{
+            userAgent: "*",
+            allow: "/",
+            disallow: "/brand"
+        }]
+    })],
+    site: "https://nilsbeerten.nl",
     output: "hybrid",
-    adapter: vercel({
-        webAnalytics: {
-            enabled: true,
-        },
-        speedInsights: {
-            enabled: true,
-        },
-    }),
+    adapter: cloudflare({
+        imageService: "passthrough"
+    })
 });
