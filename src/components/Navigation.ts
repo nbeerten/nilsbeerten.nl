3ac5e509e48aac9f86a34b8bb1e7676ec40d87be
nbeerten <59444993+nbeerten@users.noreply.github.com>
2024-01-06 15:30:22 +0100

    try to fix navbar

--- src/components/Navigation.ts
+++ src/components/Navigation.ts
@@ -6,6 +6,7 @@ type Route = {
 export function createNavigation(routes: Route[], currentUrl: string | URL) {
     const currentUrlObject = new URL(currentUrl);
     const processedRoute = routes.map((route) => {
+        console.log(new URL("/blog", "https://nilsbeerten.nl/blog").pathname === currentUrlObject.pathname);
         return {
             ...route,
             active: new URL(route.path, currentUrl).pathname === currentUrlObject.pathname,
