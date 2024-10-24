70bfb9a98e6352c2e78b8aaf5e44a09ed542c08d
nbeerten <59444993+nbeerten@users.noreply.github.com>
2024-10-24 22:36:16 +0200

    Add light mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,26 +11,26 @@ export default {
                 sans: ["Inter", ...defaultTheme.fontFamily.sans],
                 display: ["Satoshi", ...defaultTheme.fontFamily.sans],
             },
-            typography: ({ theme }: { theme: (str: string) => string }) => ({
+            typography: () => ({
                 amber: {
                     css: {
                         // Only for dark mode
-                        "--tw-prose-invert-body": theme("colors.amber[100]"),
-                        "--tw-prose-invert-headings": theme("colors.amber[100]"),
-                        "--tw-prose-invert-lead": theme("colors.amber[100]"),
-                        "--tw-prose-invert-links": theme("colors.amber[100]"),
-                        "--tw-prose-invert-bold": theme("colors.amber[100]"),
-                        "--tw-prose-invert-counters": theme("colors.amber[100]"),
-                        "--tw-prose-invert-bullets": theme("colors.amber[100]"),
-                        "--tw-prose-invert-hr": theme("colors.amber[100]/25%"),
-                        "--tw-prose-invert-quotes": theme("colors.amber[100]"),
-                        "--tw-prose-invert-quote-borders": theme("colors.stone[900]"),
-                        "--tw-prose-invert-captions": theme("colors.amber[400]"),
-                        "--tw-prose-invert-code": theme("colors.amber[100]"),
-                        "--tw-prose-invert-pre-code": theme("colors.amber[300]"), // Does not work, astro highlighting takes over
-                        "--tw-prose-invert-pre-bg": "rgb(0 0 0 / 50%)", // Does not work, astro highlighting takes over
-                        "--tw-prose-invert-th-borders": theme("colors.amber[100]/25%"),
-                        "--tw-prose-invert-td-borders": theme("colors.stone[900]"),
+                        "--tw-prose-invert-body": "var(--brand-text)",
+                        "--tw-prose-invert-headings": "var(--brand-text)",
+                        "--tw-prose-invert-lead": "var(--brand-text)",
+                        "--tw-prose-invert-links": "var(--brand-text)",
+                        "--tw-prose-invert-bold": "var(--brand-text)",
+                        "--tw-prose-invert-counters": "var(--brand-text)",
+                        "--tw-prose-invert-bullets": "var(--brand-text)",
+                        "--tw-prose-invert-hr": "var(---brand-border)",
+                        "--tw-prose-invert-quotes": "var(--brand-text)",
+                        "--tw-prose-invert-quote-borders": "var(--brand-text)",
+                        "--tw-prose-invert-captions": "var(--brand-text)",
+                        "--tw-prose-invert-code": "var(--brand-text)",
+                        // "--tw-prose-invert-pre-code": theme("colors.amber[300]"), // Does not work, astro highlighting takes over
+                        // "--tw-prose-invert-pre-bg": "rgb(0 0 0 / 50%)", // Does not work, astro highlighting takes over
+                        "--tw-prose-invert-th-borders": "var(--brand-border)",
+                        "--tw-prose-invert-td-borders": "var(--brand-border)",
                     },
                 },
             }),
@@ -40,10 +40,6 @@ export default {
                     muted: "var(--brand-muted)",
                     background: "var(--brand-background)",
                     border: "var(--brand-border)",
-                    primary: "var(--brand-primary)",
-                    secondary: "var(--brand-secondary)",
-                    tertiary: "var(--brand-tertiary)",
-                    hover: "var(--brand-hover)",
                 })
                     .map(([key, value]) => [
                         key,
