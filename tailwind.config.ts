f4c8196720fd97bdd696203708763621b7c47df8
nbeerten <59444993+nbeerten@users.noreply.github.com>
2024-11-02 16:51:24 +0100

    Formatting + other changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,22 +15,22 @@ export default {
                 amber: {
                     css: {
                         // Only for dark mode
-                        "--tw-prose-invert-body": "var(--brand-text)",
-                        "--tw-prose-invert-headings": "var(--brand-text)",
-                        "--tw-prose-invert-lead": "var(--brand-text)",
-                        "--tw-prose-invert-links": "var(--brand-text)",
-                        "--tw-prose-invert-bold": "var(--brand-text)",
-                        "--tw-prose-invert-counters": "var(--brand-text)",
-                        "--tw-prose-invert-bullets": "var(--brand-text)",
-                        "--tw-prose-invert-hr": "var(---brand-border)",
-                        "--tw-prose-invert-quotes": "var(--brand-text)",
-                        "--tw-prose-invert-quote-borders": "var(--brand-text)",
-                        "--tw-prose-invert-captions": "var(--brand-text)",
-                        "--tw-prose-invert-code": "var(--brand-text)",
+                        "--tw-prose-body": "var(--brand-text)",
+                        "--tw-prose-headings": "var(--brand-text)",
+                        "--tw-prose-lead": "var(--brand-text)",
+                        "--tw-prose-links": "var(--brand-text)",
+                        "--tw-prose-bold": "var(--brand-text)",
+                        "--tw-prose-counters": "var(--brand-text)",
+                        "--tw-prose-bullets": "var(--brand-text)",
+                        "--tw-prose-hr": "var(---brand-border)",
+                        "--tw-prose-quotes": "var(--brand-text)",
+                        "--tw-prose-quote-borders": "var(--brand-text)",
+                        "--tw-prose-captions": "var(--brand-text)",
+                        "--tw-prose-code": "var(--brand-text)",
                         // "--tw-prose-invert-pre-code": theme("colors.amber[300]"), // Does not work, astro highlighting takes over
                         // "--tw-prose-invert-pre-bg": "rgb(0 0 0 / 50%)", // Does not work, astro highlighting takes over
-                        "--tw-prose-invert-th-borders": "var(--brand-border)",
-                        "--tw-prose-invert-td-borders": "var(--brand-border)",
+                        "--tw-prose-th-borders": "var(--brand-border)",
+                        "--tw-prose-td-borders": "var(--brand-border)",
                     },
                 },
             }),
@@ -40,6 +40,7 @@ export default {
                     muted: "var(--brand-muted)",
                     background: "var(--brand-background)",
                     border: "var(--brand-border)",
+                    hr: "var(--brand-hr)",
                 })
                     .map(([key, value]) => [
                         key,
