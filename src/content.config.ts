58010a65f052fa6ef87c68f1095be370e824c35e
nbeerten <59444993+nbeerten@users.noreply.github.com>
2024-12-05 12:24:48 +0100

    Astro V5 attempt

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,8 @@
 import { z, defineCollection } from "astro:content";
+import { glob } from 'astro/loaders';
 
-const blogCollection = defineCollection({
-    type: "content",
+const blog = defineCollection({
+    loader: glob({ pattern: '**\/*.md', base: "./src/blog" }),
     schema: z.object({
         title: z.string(),
         date: z.date(),
@@ -12,5 +13,5 @@ const blogCollection = defineCollection({
 });
 
 export const collections = {
-    blog: blogCollection,
+    blog,
 };
