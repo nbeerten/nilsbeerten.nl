6ef1006c9388fa146d9e11efb019340336f63b62
nbeerten <59444993+nbeerten@users.noreply.github.com>
2026-03-15 13:12:32 +0100

    Inverse for pfp

--- src/pages/pfp.svg.ts
+++ src/pages/pfp.svg.ts
@@ -1,6 +1,7 @@
 export function GET({ request }: { request: Request }) {
     const { searchParams } = new URL(request.url);
     const round: boolean = typeof searchParams.get("round") === "string";
+    const inverse: boolean = typeof searchParams.get("inverse") === "string";
 
     /**
      * Generates an SVG path string for a wavy circle using the same
@@ -78,31 +79,25 @@ export function GET({ request }: { request: Request }) {
     const path = generateWavyCirclePath(256, 256, 225, 12, 6.5);
 
     const output = `<svg width="512" height="512" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
-        <g id="svgGroup" stroke-linecap="round" fill-rule="evenodd">
-            <rect class="background_back" width="512" height="512" rx="${round ? "512" : "0"}" ry="${round ? "512" : "0"}" />
+        <g stroke-linecap="round" fill-rule="evenodd">
+            <rect class="accent" width="512" height="512" rx="${round ? "512" : "0"}" ry="${round ? "512" : "0"}" />
             <path class="background" d="${path}" />
-            <path  class="text" transform="translate(0, -10)" d="m145 281.5v68.1h-31.2v-126.5h28.9l2.6 16.4q5.9-9.5 16.6-14.8 10.8-5.4 23.8-5.4 23.6 0 35.8 13.8 12.3 13.8 12.3 38.7v77.8h-31.2v-70.4q0-15.6-7-23.6-7-7.9-19.6-7.9-14.3 0-22.7 9.2-8.3 9.2-8.3 24.6zm150.5 48.4-2 19.7h-28.9v-190.5h31.2v82.4q6.1-10.5 18-16.5 11.9-6 26.2-6 26.6 0 42.4 18.8 15.7 18.8 15.7 49.3 0 29.4-16.5 47.6-16.5 18.2-43.6 18.2-14.3 0-25.6-6.1-11.3-6.1-16.9-16.9zm10.2-71.9q9.7-10.8 25.9-10.8 16.1 0 25.6 10.8 9.5 10.8 9.5 27.9 0 17.2-9.3 27.8-9.3 10.6-25.7 10.6-16.1 0-25.9-10.6-9.7-10.6-9.7-27.8 0-17.2 9.7-27.9z" />
+            <path class="accent" transform="translate(0, -10)" d="m145 281.5v68.1h-31.2v-126.5h28.9l2.6 16.4q5.9-9.5 16.6-14.8 10.8-5.4 23.8-5.4 23.6 0 35.8 13.8 12.3 13.8 12.3 38.7v77.8h-31.2v-70.4q0-15.6-7-23.6-7-7.9-19.6-7.9-14.3 0-22.7 9.2-8.3 9.2-8.3 24.6zm150.5 48.4-2 19.7h-28.9v-190.5h31.2v82.4q6.1-10.5 18-16.5 11.9-6 26.2-6 26.6 0 42.4 18.8 15.7 18.8 15.7 49.3 0 29.4-16.5 47.6-16.5 18.2-43.6 18.2-14.3 0-25.6-6.1-11.3-6.1-16.9-16.9zm10.2-71.9q9.7-10.8 25.9-10.8 16.1 0 25.6 10.8 9.5 10.8 9.5 27.9 0 17.2-9.3 27.8-9.3 10.6-25.7 10.6-16.1 0-25.9-10.6-9.7-10.6-9.7-27.8 0-17.2 9.7-27.9z" />
         </g>
         <style>
-            .background_back {
-                fill: #ffe7b3;
+            .accent {
+                fill: ${inverse ? "#111110" : "#ffe7b3"};
             }
             .background {
-                fill: #111110;
-            }
-            .text {
-                fill: #ffe7b3;
+                fill: ${inverse ? "#ffe7b3" : "#111110"};
             }
 
             @media (prefers-color-scheme: light) {
-                .background_back {
-                    fill: #21201c;
+                .accent {
+                    fill: ${inverse ? "#fefdfb" : "#111110"};
                 }
                 .background {
-                    fill: #fefdfb;
-                }
-                .text {
-                    fill: #21201c;
+                    fill: ${inverse ? "#111110" : "#fefdfb"};
                 }
             }
         </style>
